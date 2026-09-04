0e6d4126d49473117986ee1e622503a8655a5aa2
Artem Palaguta <art.pal@inbox.ru>
2026-09-04 21:01:29 +0300

    Update parser.py
    first save commit before parser sharing

--- parser.py
+++ parser.py
@@ -3,23 +3,40 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-moexurl = "https://iss.moex.com/iss/engines/stock/markets/shares/securities/SBER/candles.json"
 
-params =  {
-    "from": "2025-01-01",
-    "till": "2026-09-01",
-    "interval": 24
-}
+def load_data_moex(ticker, data_from, data_till):
+    moexurl = f"https://iss.moex.com/iss/engines/stock/markets/shares/securities/{ticker}/candles.json"
 
-response = requests.get(moexurl, params=params)
-response.raise_for_status()
+    params = {
+        "from": data_from,
+        "till": data_till,
+        "interval": 24
+    }
 
-data = response.json()["candles"]
+    response = requests.get(moexurl, params=params)
+    response.raise_for_status()
 
-df = pd.DataFrame(
-    data["data"],
-    columns=data["columns"]
-)
+    data = response.json()["candles"]
+
+    df = pd.DataFrame(
+        data["data"],
+        columns=data["columns"]
+    )
+
+    df["ticker"] = ticker
+
+    return df
+
+tickers = ["SBER", "GAZP", "LKOH", "GMKN", "YDEX"]
+
+full_data = []
+
+for ticker in tickers:
+    df_ticker = load_data_moex(ticker, "2025-01-01", "2026-09-04")
+    full_data.append(df_ticker)
+
+    df = pd.concat(full_data)
+
 
 ##EDA
 
@@ -27,7 +44,9 @@ df = pd.DataFrame(
 #print(df.shape)
 #print(df.describe())
 
-if df.count().isnull().sum() > 0:
-    df_cleaned = df.fillna(df.mean())
-df_duplicates = df.drop_duplicates()
+df = df.dropna()
+df = df.drop_duplicates()
+
+print(df.groupby("ticker").size())
+print(df)
 
